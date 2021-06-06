2c4ee0b3636ee206b2320c4cfd6fa59f3e3757ac
YasinVeliyev <veliyev.yasin@gmail.com>
2021-06-06 19:51:02 +0400

    new file:   ES6class/appEs5.js 	new file:   ES6class/appEs6.js 	new file:   ES6class/img/1.jpg 	new file:   ES6class/index.html 	new file:   ES6class/style.css

---
+++ ES6class/appEs6.js
@@ -0,0 +1,78 @@
+class Course{
+    constructor(title, instructor, image){
+        this.title = title;
+        this.instructor = instructor;
+        this.image = image;
+    }  
+}
+
+class UI{
+    addCourseToList(course){
+        let list = document.querySelector("#course-list")
+        let html = `
+            <tr>
+                <td><img src="img/${course.image}" style="width:100px"></td>
+                <td>${course.title}</td>
+                <td>${course.instructor}</td>
+                <td><a href="#" class ="btn btn-danger btn-sm delete">Delete</a></td>
+            </tr>   
+        `
+        list.innerHTML += html
+    }
+
+    clearControls(){
+        let title = document.getElementById('title').value = ''
+        let instructor = document.getElementById('instructor').value = ''
+        let image = document.getElementById('image').value = ''
+    
+    }
+
+    deleteCourse(element){
+        element.parentElement.parentElement.remove()
+    }
+
+    showAlert(message, className){
+        let alert = `
+            <div class ="alert alert-${className}">
+                ${message}
+            </div>
+        `;
+        let row = document.querySelector('.row')
+        row.insertAdjacentHTML('beforeBegin', alert)
+        setTimeout(function(){
+            document.querySelector('.alert').remove()
+        },1000)
+    }
+}
+
+
+let form = document.getElementById('new-course')
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let instructor = document.getElementById('instructor').value;
+    let image = document.getElementById('image').value;
+   
+    
+    let course = new Course(title, instructor, image);
+    let ui = new UI()
+    
+    if(title === ''||instructor===''||image===''){
+        ui.showAlert('Please complete the form','warning')
+    }
+    else {
+        ui.addCourseToList(course)
+        ui.clearControls()
+        ui.showAlert('Course has been added','success')
+    }
+    
+})
+
+
+document.getElementById('course-list').addEventListener('click',function(e){
+    let ui = new UI()
+    if(e.target.classList.contains('delete')){
+        ui.deleteCourse(e.target)
+        ui.showAlert('Course has been deleted','danger')
+    }
+})
