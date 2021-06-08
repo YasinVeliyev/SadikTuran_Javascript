ded938e02702ab4618209ad6c43fee957a0ebd0e
YasinVeliyev <veliyev.yasin@gmail.com>
2021-06-08 22:47:37 +0400

    modified:   promise.js

--- promise.js
+++ promise.js
@@ -8,3 +8,12 @@ let p = new Promise(function(resolve, reject){
 })
 
 p.then(msg=>console.log(msg),err=>console.log(err))
+
+new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        resolve(5)
+    },1000)
+}).then(number=>{
+    console.log(number)
+    return number * number
+}).then(number=>console.log(number))
