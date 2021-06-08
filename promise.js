7f8efc84c03711ae491ace4b73619873cb5a5bf6
YasinVeliyev <veliyev.yasin@gmail.com>
2021-06-08 22:58:45 +0400

    modified:   promise.js

--- promise.js
+++ promise.js
@@ -17,3 +17,34 @@ new Promise(function(resolve,reject){
     console.log(number)
     return number * number
 }).then(number=>console.log(number))
+
+
+let isMomHappy = true;
+
+let willGetNewPhone = new Promise((resolve,reject)=>{
+    if(isMomHappy){
+        let phone = {
+            name:'Iphone 8',
+            price:4000,
+            color:'silver'
+        }
+        resolve(phone)
+    }
+    else {
+        let error = new Error('Mom is not happy')
+        reject(error)
+    }
+})
+
+const showToFriends = (phone) =>{
+    const message = "Hi friends this my new phone "+phone.name;
+    return Promise.resolve(message)
+}
+
+let askMom = () => {
+    willGetNewPhone
+    .then(showToFriends)
+    .then(console.log)
+    .catch(console.log)
+}
+askMom()
